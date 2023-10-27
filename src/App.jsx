42dcdafe000aebfc8efa1e9e7ed787ea143c1650
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-10-27 16:36:10 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import batPNG from './assets/bat-png.png'
+import webPNG from './assets/spider-web.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div class="full-screen-image"></div>
+
+    </>
+  )
+}
+
+export default App
